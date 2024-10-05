0916e6fb3bbad89dd552bb20a6dcf7a53094ae46
Rebah Rachael <138818731+chestar-rachael@users.noreply.github.com>
2024-10-05 10:55:38 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+function runClock(){
+  const now = new Date();
+  const hours = now.getHours().toString().padStart(2,0);
+  const minutes = now.getMinutes().toString().padStart(2,0);
+  const seconds = now.getSeconds().toString().padStart(2,0);
+  const time = `${hours}:${minutes}:${seconds}`;
+  document.getElementById("clock").textContent = time;
+}
+runClock()
+setInterval(runClock,1000)
